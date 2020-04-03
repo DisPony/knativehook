67cfca8d74e3e63b3caa57c4f1b718db616c59af
Nikita Klimenko <nikitaklimenko2000@gmail.com>
2020-04-03 03:17:19 +0300

    Delete deprecated adapters

--- src/main/kotlin/KeyListener.kt
+++ src/main/kotlin/KeyListener.kt
@@ -1,49 +1,13 @@
 import events.NativeEvents
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.runBlocking
 import org.jnativehook.GlobalScreen
 import org.jnativehook.keyboard.NativeKeyAdapter
 import org.jnativehook.keyboard.NativeKeyEvent
-import org.jnativehook.keyboard.NativeKeyListener
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
-
-class KeyExtension(capacity: Int = Channel.BUFFERED) : NativeKeyListener {
-
-    val keyTypedChannel = Channel<NativeKeyEvent>(capacity)
-    val keyPressedChannel = Channel<NativeKeyEvent>(capacity)
-    val keyReleasedChannel = Channel<NativeKeyEvent>(capacity)
-
-    override fun nativeKeyTyped(e: NativeKeyEvent) = runBlocking {
-        keyTypedChannel.send(e)
-    }
-
-    override fun nativeKeyPressed(e: NativeKeyEvent) = runBlocking {
-        keyPressedChannel.send(e)
-    }
-
-    override fun nativeKeyReleased(e: NativeKeyEvent) = runBlocking {
-        keyReleasedChannel.send(e)
-    }
-
-}
-
-class KeyProvider(private val listener: KeyExtension) {
-
-    /*
-    For some reason doesn't work on my system
-     */
-    suspend fun getKeyType(): NativeKeyEvent = listener.keyTypedChannel.receive()
-
-    suspend fun getKeyPress(): NativeKeyEvent = listener.keyPressedChannel.receive()
-
-    suspend fun getKeyRelease(): NativeKeyEvent = listener.keyReleasedChannel.receive()
-
-}
 
 /*
 For some reason doesn't work on my system
--- src/main/kotlin/MouseMotionListener.kt
+++ src/main/kotlin/MouseMotionListener.kt
@@ -1,39 +1,14 @@
 import events.NativeEvents
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.runBlocking
 import org.jnativehook.GlobalScreen
 import org.jnativehook.mouse.NativeMouseEvent
 import org.jnativehook.mouse.NativeMouseMotionAdapter
-import org.jnativehook.mouse.NativeMouseMotionListener
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
-class MouseMotionExtension(capacity: Int = Channel.BUFFERED): NativeMouseMotionListener {
-
-    val mouseMovedChannel = Channel<NativeMouseEvent>(capacity)
-    val mouseDraggedChannel = Channel<NativeMouseEvent>(capacity)
-
-    override fun nativeMouseMoved(e: NativeMouseEvent) = runBlocking {
-        mouseMovedChannel.send(e)
-    }
-
-    override fun nativeMouseDragged(e: NativeMouseEvent) = runBlocking {
-        mouseDraggedChannel.send(e)
-    }
-
-}
-
-class MouseMotionProvider(private val listener: MouseMotionExtension) {
-
-    suspend fun getMouseMove(): NativeMouseEvent = listener.mouseMovedChannel.receive()
-
-    suspend fun getMouseDrag(): NativeMouseEvent = listener.mouseDraggedChannel.receive()
-
-}
 
 suspend fun NativeEvents.Companion.getMouseMove(): NativeMouseEvent = suspendCoroutine { cont ->
     val callback = object : NativeMouseMotionAdapter() {
